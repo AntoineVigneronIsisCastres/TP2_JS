7f0d580e46a8c8fc4626ee3bb33b30ee5ab88b06
AntoineVigneronIsisCastres <antoine.vigneron@etud.univ-jfc.fr>
2022-03-23 23:05:20 +0100

    Display console queries almost done

--- tp2.js
+++ tp2.js
@@ -1,1 +1,6 @@
 console.log("bonjour");
+console.log(document.getElementById("c1"));
+console.log(document.getElementById("j1").firstElementChild);
+console.log(document.getElementsByClassName("carte"));
+console.log(document.getElementsByTagName("p"));
+console.log(document.getElementById("j1").className);
